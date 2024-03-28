f41320799bc85a59e8f372ead9cbc85802b8255f
LohRZ <B022220028@student.utem.edu.my>
2024-03-28 15:19:44 +0800

    commit change

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.listen(port, () => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://ahzhe900:Fuckyou69420@cluster0.w4psvcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,11 +30,47 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+    let result = await client.db('sample_mflix').collection('info').find(
+        { name: 'Waddup'}
+    ).toArray()
+
+   // updateOne(
+     // {_id: new ObjectId('6605103d2d035a66e70704ee')},
+     // { $set: 
+       // { age: '21',
+        //  status: 'A'
+        //}
+      //}
+    //)
+
+    //insertOne({
+      //name: 'Waddup',
+      //age: '22',
+      //status: 'single',
+      //faculty: 'FTKEK',
+    //})
+
+    //.find(
+      //{ name: 'Waddup'}
+  //).toArray()
+
+    // .find().toArray() is find
+    // .insertOne is insert
+    // .updateOne is update
+    // .deleteOne is delete
+
+    console.log(result)
+    //console.log is a log for showing that ive successfully insered the 'result' into the database
+
+
+    //await client.db("admin").command({ ping: 1 });
+    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
